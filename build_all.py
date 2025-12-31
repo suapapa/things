62ff69cc605673fe5120747dfd72b3333d6c994d
Homin Lee <homin.crc+dev@gmail.com>
2026-01-01 07:50:39 +0900

    build: use docker for openscad stl conversion

--- build_all.py
+++ build_all.py
@@ -5,6 +5,13 @@ import glob, os
 for f in glob.glob('*/*.scad'):
     o = f.replace('.scad', '.stl')
     print('converting {input} to {output}...'.format(input=f, output=o))
-    os.system('openscad -o {output} {input}'.format(input=f, output=o))
+    cmd = 'docker run --rm -v "{cwd}":/openscad -w /openscad -u {uid}:{gid} openscad/openscad:latest openscad -o "{output}" "{input}"'.format(
+        cwd=os.getcwd(),
+        uid=os.getuid(),
+        gid=os.getgid(),
+        input=f,
+        output=o
+    )
+    os.system(cmd)
 
 print('all done')
